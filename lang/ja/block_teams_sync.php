555835645236c7cb9f5bb367bd06e6aeb9675936
ht-itoh-nitech <63279196+ht-itoh-nitech@users.noreply.github.com>
2020-06-05 15:12:34 +0900

    second commit
    Add line 8

--- lang/ja/block_teams_sync.php
+++ lang/ja/block_teams_sync.php
@@ -5,4 +5,4 @@ $string['teams_sync'] = 'Teams同期';
 $string['teamson'] = 'このコースは Teams と同期されています。';
 $string['teamsoff'] = 'このコースは Teams と同期されていません。';
 $string['turnon'] = '同期する';
-
+$string['turnoff'] = '同期を解除する';
