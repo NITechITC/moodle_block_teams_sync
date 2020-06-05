e5f51e0c212ef841731fc2ff371d063aa6ad6878
ht-itoh-nitech <63279196+ht-itoh-nitech@users.noreply.github.com>
2020-06-05 15:11:29 +0900

    second commit
    Add line 8

--- lang/en/block_teams_sync.php
+++ lang/en/block_teams_sync.php
@@ -5,4 +5,4 @@ $string['teams_sync'] = 'Teams_Sync';
 $string['teamsoff'] = 'Teams Sync of this course is OFF.';
 $string['teamson'] = 'Teams Sync of this course is ON.';
 $string['turnon'] = 'Turn on.';
-
+$string['turnoff'] = 'Turn OFF.';
